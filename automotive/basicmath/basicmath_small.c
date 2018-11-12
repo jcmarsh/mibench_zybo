3371ff753f1283974f84a7d33cf2070401295a1c
James Marshall <jcmarsh@gwmail.gwu.edu>
2018-11-11 21:18:46 -0500

    Increase basicmath parameters.

--- automotive/basicmath/basicmath_small.c
+++ automotive/basicmath/basicmath_small.c
@@ -8,7 +8,7 @@
 
 /* The printf's may be removed to isolate just the math calculations */
 
-#define CUBIC_NUM (4 + (10 * 4))
+#define CUBIC_NUM (4 + (10 * 20))
 int cubic_index = 0;
 int cubic_solutions[CUBIC_NUM] = {0};
 double cubic_x[CUBIC_NUM * 3] = {0.0};
@@ -67,8 +67,7 @@ int single_run(void)
   a1 = 1;
   //for(b1=10;b1>0;b1--) {
   b1 = 10;
-  //for(c1=5;c1<15;c1+=0.5) {
-  for(c1=5;c1<7;c1+=0.5) {
+  for(c1=5;c1<15;c1+=0.5) {
     for(d1=-1;d1>-11;d1--) {
       SolveCubic(a1, b1, c1, d1, &(cubic_solutions[cubic_index]), &(cubic_x[cubic_index * 3]));
       cubic_index++;
